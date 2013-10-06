1b1e727be9fd892a67c3f825f668b15b24594ca8
kanj <knjterrell@mybluelight.com>
2013-10-06 08:21:11 -0700

    Remove unsupported BELFS Signed-off-by: kanj <knjterrell@mybluelight.com>

--- BELFS/makefiles/_includes.mk
+++
@@ -1,6 +0,0 @@
-include makefiles/babl.mk
-include makefiles/libogg.mk
-include makefiles/libsamplerate.mk
-include makefiles/libsndfile.mk
-include makefiles/mpg123.mk
-include makefiles/slang.mk
--- BELFS/scripts/aalib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-source /.bashrc
-cd /sources/aalib-1.2
-./configure --prefix=/usr \
-            --infodir=/usr/share/info \
-            --mandir=/usr/share/man \
-            --disable-static && make
